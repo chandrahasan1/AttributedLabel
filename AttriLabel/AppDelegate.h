0b6a3e4c61797b0f073856d75cb35f59c15f814c
Chandu <apple@Shyams-MBP.local>
2013-08-05 14:00:12 +0530

    Initial Commit

---
+++ AttriLabel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AttriLabel
+//
+//  Created by Chandu on 8/5/13.
+//  Copyright (c) 2013 Fab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AttriLabel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AttriLabel
+//
+//  Created by Chandu on 8/5/13.
+//  Copyright (c) 2013 Fab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
